5d065dcd0fd92d6fe29e564cf54e02cf9a1db03c
Dale Lukas Peterson <hazelnusse@gmail.com>
2010-08-25 15:22:00 -0700

    Modified the plot.

--- holonomic_constraint.py
+++ holonomic_constraint.py
@@ -13,8 +13,8 @@ ra = rb = 1.0
 l = 1.5
 alpha = np.pi/2.0
 
-q2 = np.linspace(-np.pi, np.pi, 100)
-q3 = np.linspace(-np.pi, np.pi, 100)
+q2 = np.linspace(-np.pi/2.0, np.pi/2.0, 100)
+q3 = np.linspace(-np.pi, np.pi, 200)
 Q2, Q3 = np.meshgrid(q2, q3)
 hc = (rb*(1.0-(np.cos(alpha)*np.sin(Q2)+np.sin(alpha)*np.cos(Q2)*np.cos(Q3))**2.0)**0.5
     - l*np.cos(Q2)*np.cos(Q3) - ra*np.cos(Q2)**2.0/np.abs(np.cos(Q2)))
@@ -22,5 +22,8 @@ hc = (rb*(1.0-(np.cos(alpha)*np.sin(Q2)+np.sin(alpha)*np.cos(Q2)*np.cos(Q3))**2.
 plt.figure()
 CS = plt.contour(Q2, Q3, hc, levels=[0])
 plt.clabel(CS, inline=1, fontsize=10)
+plt.xlabel("Lean (q2), radians")
+plt.ylabel("Spin (q3), radians")
+plt.axis([-np.pi/2.0, np.pi/2.0, -np.pi, np.pi])
 
 plt.show()
