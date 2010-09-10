52f419952be4ebc15744087439acec5e8593b1c4
Dale Lukas Peterson <hazelnusse@gmail.com>
2010-09-09 21:23:33 -0700

    Modified the constraint and parameter values.
    Ensured that the simulation is always acting on the holonomic constraint level
    curve.

--- holonomic_constraint.py
+++ holonomic_constraint.py
@@ -2,22 +2,28 @@
 
 import matplotlib
 import numpy as np
+from numpy import sin, cos, abs
 import matplotlib.cm as cm
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
+from plotcpp import cpp_record_dt
+import os
 
-matplotlib.rcParams['xtick.direction'] = 'out'
-matplotlib.rcParams['ytick.direction'] = 'out'
+w3 = 1.0
+tf = 2.0
+os.system('rm simulatecpp.dat')
+os.system('./sim {0} {1} > simulation_settings.txt'.format(w3, tf))
+data = np.fromfile('./simulatecpp.dat', dtype=cpp_record_dt)
 
-ra = rb = 1.0
-l = 1.5
+ra = .1
+rb = .1
+l = np.sqrt(2.0) * ra
 alpha = np.pi/2.0
 
 q2 = np.linspace(-np.pi/2.0, np.pi/2.0, 100)
 q3 = np.linspace(-np.pi, np.pi, 200)
 Q2, Q3 = np.meshgrid(q2, q3)
-hc = (rb*(1.0-(np.cos(alpha)*np.sin(Q2)+np.sin(alpha)*np.cos(Q2)*np.cos(Q3))**2.0)**0.5
-    - l*np.cos(Q2)*np.cos(Q3) - ra*np.cos(Q2)**2.0/np.abs(np.cos(Q2)))
+hc = rb*(cos(Q2)**2*cos(Q3)**2+(sin(alpha)*sin(Q2)-cos(alpha)*sin(Q3)*cos(Q2))**2)/(1-(cos(alpha)*sin(Q2)+sin(alpha)*sin(Q3)*cos(Q2))**2)**0.5 - l*cos(Q2)*cos(Q3) - ra*cos(Q2)**2/abs(cos(Q2))
 
 plt.figure()
 CS = plt.contour(Q2, Q3, hc, levels=[0])
@@ -26,4 +32,8 @@ plt.xlabel("Lean (q2), radians")
 plt.ylabel("Spin (q3), radians")
 plt.axis([-np.pi/2.0, np.pi/2.0, -np.pi, np.pi])
 
-plt.show()
+plt.hold(True)
+plt.plot(data[:]['q2'], data[:]['q3'], 'r.')
+
+
+plt.savefig('holonomic_constraint.pdf')
